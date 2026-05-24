31a74bdf4b250b178cbe04ba1fdaa4aa206f841b
Mustavojj <mostavojj@gmail.com>
2026-05-24 04:04:45 +0300

    Add databaseURL to Firebase configuration

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBDDxrUoWF8bXpUTu7DPsVOxJTBW6mFM8g",
   authDomain: "veltrix-apps.firebaseapp.com",
+  databaseURL: "https://veltrix-apps-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "veltrix-apps",
   storageBucket: "veltrix-apps.firebasestorage.app",
   messagingSenderId: "624237984093",
